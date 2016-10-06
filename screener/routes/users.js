f7f8cc3d797f9c0564f4e0f6faf6d2223937262a
Mahesh Sasidharan <Mahesh.Sasidharan.1990@gmail.com>
2016-10-06 09:32:01 -0500

    Access database and send response to client

--- screener/routes/users.js
+++ screener/routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
 /* GET users listing. */
@@ -7,8 +8,38 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/test', function(req, res, next) {
-  res.send('This is from test');
+	handle_database(req,res);
+	//res.send('This is from test');
 });
+
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config')[env];
+
+var pool = mysql.createPool(config.poolConfig);
+
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query("select * from User",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }           
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
+}
 
 
 module.exports = router;
